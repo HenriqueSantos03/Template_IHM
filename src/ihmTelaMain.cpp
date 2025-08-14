da22e841fcecb0d537630c1525cfeb2c54875f30
HenriqueSantos03 <henrique.santos.tron@gmail.com>
2025-08-14 15:44:34 -0300

    Pre alterações organização

--- src/ihmTelaMain.cpp
+++ src/ihmTelaMain.cpp
@@ -9,10 +9,8 @@
 
 LGFX gfx;
 
-static const uint16_t screenWidth = 800;
-static const uint16_t screenHeight = 480;
 static lv_disp_draw_buf_t draw_buf;
-static lv_color_t buf[screenWidth * screenHeight / 10];
+static lv_color_t buf[SCREEN_WIDTH * SCREEN_HEIGHT / 10];
 
 void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
     uint32_t w = (area->x2 - area->x1 + 1);
@@ -43,21 +41,15 @@ void ihmTelaMainInit() {
     gfx.setBrightness(255);  // Backlight máximo
     Serial.println("Display inicializado");
 
-    gfx.fillScreen(0xF800);  // Vermelho
-    Serial.println("Tela deve estar vermelha");
-    delay(2000);
-    gfx.fillScreen(0x0000);  // Preto
-    Serial.println("Tela deve estar preta");
-
     Serial.println("Inicializando LVGL...");
     lv_init();
-    lv_disp_draw_buf_init(&draw_buf, buf, NULL, screenWidth * screenHeight / 10);
+    lv_disp_draw_buf_init(&draw_buf, buf, NULL, SCREEN_WIDTH * SCREEN_HEIGHT / 10);
     Serial.println("Buffer LVGL inicializado");
 
     static lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
-    disp_drv.hor_res = screenWidth;
-    disp_drv.ver_res = screenHeight;
+    disp_drv.hor_res = SCREEN_WIDTH;
+    disp_drv.ver_res = SCREEN_HEIGHT;
     disp_drv.flush_cb = my_disp_flush;
     disp_drv.draw_buf = &draw_buf;
     lv_disp_drv_register(&disp_drv);
@@ -70,26 +62,19 @@ void ihmTelaMainInit() {
     lv_indev_drv_register(&indev_drv);
     Serial.println("Driver de toque registrado");
 
-    // Criar botão
+    // Criar botão 1
     lv_obj_t *btn_1 = lv_btn_create(lv_scr_act());
-    if (!btn_1) {
-        Serial.println("Erro: Falha ao criar botão");
-        return;
-    }
-    lv_obj_set_size(btn_1, 200, 100);
-    lv_obj_align(btn_1, LV_ALIGN_CENTER, 0, 0);
-
+    lv_obj_set_size(btn_1, 200, 100); // Definir tamanho do botão
+    lv_obj_align(btn_1, LV_ALIGN_CENTER, 0, 0); // Centralizar o botão
+    
+    // Criar label do botão 1
     lv_obj_t *label = lv_label_create(btn_1);
-    if (!label) {
-        Serial.println("Erro: Falha ao criar label");
-        return;
-    }
     lv_label_set_text(label, "Clique Aqui");
     lv_obj_center(label);
-    Serial.println("Botão criado");
-
+    
     // Registrar botão para eventos
-    btn_1_event_cb(btn_1);
+    btn_1_event_cb(btn_1); 
+
 }
 
 void ihmTelaMainTask() {
--- src/ihmTelaMain.h
+++ src/ihmTelaMain.h
@@ -1,5 +1,11 @@
+//----------------------------------------------
+// ihmTelaMain.h (Copia de ihmDashboard.h)
+//----------------------------------------------
 #ifndef IHM_TELA_MAIN_H
 #define IHM_TELA_MAIN_H
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 480
 
 void ihmTelaMainInit();
 void ihmTelaMainTask();
